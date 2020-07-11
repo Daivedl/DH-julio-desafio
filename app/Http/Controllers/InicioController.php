86dc1ea560959f530ad35a390a2c3e36045f9291
David Leiva <daived@live.com.ar>
2020-07-10 22:07:30 -0300

    agregando un volver

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Peliculas;
+use App\Genero;
+use App\Actor;
 class InicioController extends Controller
 {
     public function __construct()
--- resources/views/busquedaTitulos.blade.php
+++ resources/views/busquedaTitulos.blade.php
@@ -1,7 +1,17 @@
 @include('header')
 @if (count($peliculas))
     @foreach ($peliculas as $item)
-        <p class="p-2 border-bottom">{{$item->title}}</p>
+        <h2 class="p-2 border-bottom">{{$item->title}}</h2>
+        @if ($item->genero)
+        <p>Género: {{$item->genero->name}}</p>
+        @endif
+        <p>Rating: {{$item->rating}}</p>
+        <p>Premios: {{$item->awards}}</p>
+        <p>Fecha de lanzamiento: {{$item->release_date}}</p>
+
     @endforeach
+@else
+<h2>No se encontraron resultados</h2>
+<a href="inicio">Volver</a>
 @endif
 @include('footer')
