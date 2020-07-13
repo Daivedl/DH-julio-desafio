5fb3d324136e535f3fda273ec4f86d38b52c337c
David Leiva <daived@live.com.ar>
2020-07-12 22:00:48 -0300

    algo de bootstrap

--- resources/views/detallePelicula.blade.php
+++ resources/views/detallePelicula.blade.php
@@ -1,27 +1,31 @@
 @include('header')
 <div class="row mt-3">
     <div class="col-8">
-    <h2>Película: {{$peliculas->title}} </h2>
+    <h2 class="mb-3">Película: {{$peliculas->title}} </h2>
 
         @if ($peliculas->genero)
-        <p>Género: {{$peliculas->genero->name}}</p>
+        <h5 class="mb-3 form-control">Género: {{$peliculas->genero->name}}</h5>
+
         @endif
-        <p>Rating: {{$peliculas->rating}}</p>
-        <p>Premios: {{$peliculas->awards}}</p>
-        <p>Fecha de lanzamiento: {{$peliculas->release_date}}</p>
+        <h5 class="mb-3 form-control">Rating: {{$peliculas->rating}}</h5>
+
+        <h5 class="mb-3 form-control">Premios: {{$peliculas->awards}}</h5>
+
+        <h5 class="mb-3 form-control">Fecha de lanzamiento: {{$peliculas->release_date}}</h5>
+
         @if ($peliculas->actores)
-        <p>Actores:</p>
+        <h5 class="mb-3 form-control">Actores:</h5>
         @endif
         <ul>
         @foreach ($peliculas->actores as $actor)
         <li>
-            {{$actor->getNombreCompleto()}}
+        <p class="form-control">{{$actor->getNombreCompleto()}}</p>
+
         </li>
         @endforeach
         </ul>
+    <a class="btn btn-primary btn-sm" href="{{url('/inicio')}}">Volver</a>
+    </div>
 
-    </div>
-    <div class="col-4">
-    </div>
 </div>
 @include('footer')
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,27 +2,37 @@
 
 <div class="row mt-3">
     <div class="col-8">
-        <h1>Películas que pueden interesarte</h1>
+        <h1 class="mb-5">Películas que pueden interesarte</h1>
+
         <ul>
+
             @forelse($peliculas as $pelicula)
-                <li>
-                {{$pelicula["title"]}}
-                <a href="{{url('/detallePelicula/'.$pelicula->id)}}">| Ver detalles</a>
+                <li class="mb-4">
+                <h4 class="d-inline form-control">{{$pelicula["title"]}}</h4>
+                <div class="col-6 d-inline">
+                <a class="btn btn-primary btn-sm d-inline" href="{{url('/detallePelicula/'.$pelicula->id)}}">Ver detalles</a>
+                </div>
 
                 </li>
+
             @empty
                 <p>No hay películas</p>
             @endforelse
+
         </ul>
 
     </div>
     <div class="col-4">
-        <h2>Últimas 5 películas</h2>
+        <h2 class="mt-2 mb-5">Últimas 5 películas</h2>
         <ul>
             @forelse($peliculasUltimas as $peliculasUltima)
-                <li>
-                {{$peliculasUltima["title"]}}
-                <a href="{{url('/detallePelicula/'.$peliculasUltima->id)}}">| Ver detalles</a>
+                <li class="mb-4">
+                <h6 class="d-inline form-control">{{$peliculasUltima["title"]}}</h6>
+
+                <div class="col-6 d-inline">
+                <a class="btn btn-primary btn-sm d-inline"href="{{url('/detallePelicula/'.$peliculasUltima->id)}}">Ver detalles</a>
+
+                </div>
 
                 </li>
             @empty
